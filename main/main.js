3f1037aa30b11169a7116ec2a64b2c121aaab8e9
MIN YAN <miny1994@163.com>
2018-09-27 19:53:46 +0800

    sum of integers in string

--- main/main.js
+++ main/main.js
@@ -1,3 +1,12 @@
-module.exports = function main() {
-  // Write your code here
+
+module.exports = function main(String) {
+var sum=0;
+  const cal=param=>{
+	  param=param.toString();
+	  for(const i in param){
+		  let num=parseInt(param[i]);
+	  };
+	  sum+=num;
+  };
+  return sum;
 };
